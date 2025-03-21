01fc5239b2042c65919414584d2463e3c5b616ef
Eli Hazan <eli@elihazan.com>
2025-03-21 17:16:09 -0400

    Switch config to CommonJS

--- capacitor.config.js
+++ capacitor.config.js
@@ -1,9 +1,16 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
+// capacitor.config.js - CommonJS format (compatible with Node)
+/// <reference types="@capacitor/cli" />  /* Optionally enable type info in VSCode */
+/** @type {import('@capacitor/cli').CapacitorConfig} */  // JSDoc for type checking
+const config = {
   appId: 'com.benandnino.driverconnect',
   appName: 'DriverConnect',
-  webDir: 'dist'
+  webDir: 'dist',        // path to built web assets
+  bundledWebRuntime: false,
+  // Example: using an environment variable (allowed in Appflow)
+  // plugins: {
+  //   LiveUpdates: {
+  //     channel: process.env.LIVE_UPDATE_CHANNEL_NAME  // dynamic config
+  //   }
+  // }
 };
-
-export default config;
+module.exports = config;
